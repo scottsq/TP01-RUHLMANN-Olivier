23e891abcecff855cd3a37c03bc1a326fe2592ba
RUHLMANN Olivier <ruhlmann.olivier@hotmail.fr>
2021-03-01 16:20:13 +0100

    [UPDATE] Changed form validation

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 $("#form").submit(e => {
-    if (!arePasswordsValid()) return false;
+    if (!validateForm()) return false;
 });
 
+function validateForm() {
+    let form = $("#form");
+    let foundError = false;
+    form.find("input").each((i, elem) => {
+        elem = $(elem);
+        let pattern = elem.attr("pattern");
+        if (pattern && !elem.val().match(new RegExp(pattern))) {
+            showDivError(elem);
+            foundError = true;
+        }
+        else if (divError) divError.addClass("hidden");
+    });
+    return foundError;
+}
+
 function arePasswordsValid() {
-    if (!$("#inputPassword").val() == $("#inputPassword2").val()) {
-        alert("Password are not the same.");
+    let pwd1 = $("#inputPassword");
+    let pwd2 = $("#inputPassword2");
+    if (!pwd1.val() == pwd2.val()) {
+        showDivError(pwd2);
     }
+}
+
+function showDivError(elem) {
+    let divError = elem.parent().find(".val-error").first(); 
+    if (divError) divError.removeClass("hidden");
 }
